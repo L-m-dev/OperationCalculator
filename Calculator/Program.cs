3f814cb60f428a3f47db0e01313729e8d8161700
Nice <lucas.muller.acad@gmail.com>
2024-08-16 17:09:38 -0300

    added While loop. Added JSON serializing.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,70 +2,79 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Text.RegularExpressions;
 using CalculatorLibrary;
-    
-decimal? num1;
-decimal? num2;
 
-decimal cleanNum1 = 0;
-decimal cleanNum2 = 0;
-
-Calculator calculator = new();
-
-Console.WriteLine("Type first number:");
-
-while(!Decimal.TryParse(Console.ReadLine(), out cleanNum1))
+while (true)
 {
-    Console.WriteLine("Invalid input. Enter valid number");
-}
+    decimal? num1;
+    decimal? num2;
 
-Console.WriteLine("Type second number:");
+    decimal cleanNum1 = 0;
+    decimal cleanNum2 = 0;
 
-while (!Decimal.TryParse(Console.ReadLine(), out cleanNum2))
-{
-    Console.WriteLine("Invalid input. Enter valid number");
-}
+    Calculator calculator = new();
 
-Console.WriteLine("Operation?\n1- Sum\n2-Minus\n3-Multiply\n4-Divide");
+    Console.WriteLine("Type first number:");
 
-string? choice = Console.ReadLine().Trim().ToLower();
-Operation? operation;
-
-if (choice == null || !Regex.IsMatch(choice, "[1|2|3|4]"))
-{
-    Console.WriteLine("Invalid choice.");
-}
-else
-{
-    try
+    while (!Decimal.TryParse(Console.ReadLine(), out cleanNum1))
     {
-        switch (choice)
+        Console.WriteLine("Invalid input. Enter valid number");
+    }
+
+    Console.WriteLine("Type second number:");
+
+    while (!Decimal.TryParse(Console.ReadLine(), out cleanNum2))
+    {
+        Console.WriteLine("Invalid input. Enter valid number");
+    }
+
+    Console.WriteLine("Operation?\n1- Sum\n2-Minus\n3-Multiply\n4-Divide");
+
+    string? choice = Console.ReadLine().Trim().ToLower();
+    Operation operation;
+
+    if (choice == null || !Regex.IsMatch(choice, "[1|2|3|4]"))
+    {
+        Console.WriteLine("Invalid choice.");
+    }
+    else
+    {
+        try
         {
-            case "1":
-                operation = Operation.Add;
-                break;
-            case "2":
-                operation = Operation.Subtract;
-                break;
-            case "3":
-                operation = Operation.Multiply;
-                break;
-            case "4":
-                operation = Operation.Divide;
-                break;
-            default:
-                throw new InvalidOperationException("Invalid choice of operation.");
+            switch (choice)
+            {
+                case "1":
+                    operation = Operation.Add;
+                    break;
+                case "2":
+                    operation = Operation.Subtract;
+                    break;
+                case "3":
+                    operation = Operation.Multiply;
+                    break;
+                case "4":
+                    operation = Operation.Divide;
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid choice of operation.");
+            }
+
+            decimal? result = calculator.DoOperation(cleanNum1, cleanNum2, operation);
+
+            Console.WriteLine(result);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
         }
 
-        decimal? result = calculator.DoOperation(cleanNum1, cleanNum2, operation);
-
-        Console.WriteLine(result);
-
+        calculator.Finish();
+        Console.WriteLine("Want to quit? type in 'quit'. Want to continue, press Enter");
+        if (Console.ReadLine().Trim().ToLower().Equals("quit"))
+        {
+            break;
+        }
     }
-    catch (Exception e)
-    {
-        Console.WriteLine(e.Message);
-    }
-
 
 }
 
