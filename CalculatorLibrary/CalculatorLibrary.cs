aea4ee073870467270b1457aa6d491c5d98af135
Nice <lucas.muller.acad@gmail.com>
2024-08-16 19:40:48 -0300

    add files

---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        JsonWriter writer;
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+        }
+
+        public decimal? DoOperation(decimal num1, decimal num2, Operation op)
+        {
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
+
+            decimal? result = 0;
+            switch (op)
+            {
+                case Operation.Add:
+                    result = num1 + num2;
+                    writer.WriteValue("Add");
+                    break;
+                case Operation.Subtract:
+                    result = num1 - num2;
+                    writer.WriteValue("Subtract");
+
+                    break;                case Operation.Multiply:
+                    result = num1 * num2;
+                    writer.WriteValue("Multiply");
+                    break;
+                case Operation.Divide:
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        writer.WriteValue("Divide");
+                        break;                    }
+                    else
+                    {
+                        num2 = 1;
+                        result = num1 / num2;
+                        writer.WriteValue("Divide");
+                        break;
+                    }
+
+                default:
+                    throw new InvalidOperationException("Invalid choice");
+            }
+
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+
+        }
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+
+    }
+
+}
+
---
+++ CalculatorLibrary/OperationEnum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public enum Operation
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide
+    }
+
+    public static class OperationEnumHelper
+    {
+        public static string OperationEnumFriendlyString(Operation op)
+        {
+            switch (op)
+            {
+                case Operation.Add:
+                    return "+";
+                case Operation.Subtract:
+                    return "-";
+                case Operation.Multiply:
+                    return "*";
+                case Operation.Divide:
+                    return "/";
+
+            }
+            throw new Exception("Invalid operation.");
+        }
+
+    }
+
+}
